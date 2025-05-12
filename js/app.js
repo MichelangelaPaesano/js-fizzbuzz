6b27816a170b6a70749f4f3f67b49d472e216986
MichelangelaPaesano <paesanomichelangela@gmail.com>
2025-05-12 19:02:09 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//scrivere numeri da 1 a 100
+for (let i = 0; i < 100; i++) {
+    const num = i + 1
+    console.log (num)
+}
+   
+
+// IF multipli di 3
+// - stampiamo la parola "Fizz"
+// IF multipli di 5
+// - stampiamo la parola "Buzz"
+// IF multipli di 3 e 5 
+// - stampiamo la parola "FizzBuzz"
+// ALTRIMENTI:
+// - stampiamo la cifra 
